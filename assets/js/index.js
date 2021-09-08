bf5e88ceabdd0bf0619139de5ca9bf9910f615b4
NaumenkoDmitry <55960625+NaumenkoDmitry@users.noreply.github.com>
2021-09-08 15:52:29 +0300

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -130,8 +130,6 @@ function stringToColour(str) {
 }
 
 function generateLinks(contacts) {
-  // пройтись мапом выернуть уже готовые элементы ссылок
-  // воспользоватся мапой для определния какую картинку делать
   const linksArray = contacts.map((contact) => {
     const url = new URL(contact);
     const hostname = url.hostname;
@@ -139,6 +137,7 @@ function generateLinks(contacts) {
     if (SUPPORTED_SOCIAL_NETWORKS.has(hostname)) {
       const link = createElement("a", {
         classNames: SUPPORTED_SOCIAL_NETWORKS.get(hostname),
+        attrs: { id: `a-class` },
       });
       link.setAttribute("href", url);
       return link;
